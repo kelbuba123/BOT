b07bccbb0d9fbf9db6c6cfdec9d08ab073ce9fad
kelbuba123 <43009695+kelbuba123@users.noreply.github.com>
2018-09-06 12:34:48 +0300

    Add files via upload

---
+++ komutlar/vote.js
@@ -0,0 +1,38 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Oylama Başlatmak için Administratör Yetkiniz Olması Lazım');
+        if(!args[0]) return message.channel.send('Kullanımı : s!oylama {Oylama Sorusu}');
+        
+        const oyembed = new Discord.RichEmbed()
+        .setColor(0xffffff)
+        .setFooter('Oy Kullanabilmek için Tepki Ver')
+        .setDescription(args.join(' '))
+        .setTitle(`Oylamayı Oluşturan: ${message.author.username}`);
+
+        let msg = await message.channel.send(oyembed)
+        .then(function (msg) {
+            msg.react("👍");
+            msg.react("👎");
+            
+            }).catch(function(error) {
+            console.log(error);
+        });
+
+
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 2
+};
+
+
+
+module.exports.help = {
+  name:"oylama"
+}
