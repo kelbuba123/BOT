3ebf4d1a51d831cc2a022ecc402c0f94fff56c26
kelbuba123 <43009695+kelbuba123@users.noreply.github.com>
2018-09-08 12:05:47 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,33 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**!');
-    member.guild.channel.defaultChannel('hg '+username+'');
-};
+const fs = require("fs");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./ayarlar.json");
+
+client.login(ayarlar.token);
+
+client.on("ready", () => {
+  client.user.setGame(`on ${client.guilds.size} servers`);
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
+});
+
+client.on("guildMemberAdd", (member) => {
+	console.log(`New User ${member.user.username} has joined ${member.guild.name}` );
+	member.guild.defaultChannel.send(`${member.user} Sunucumuza Katıldı ! Hoşgeldin Dostum`);
+});
+
+client.on("message", (message) => {
+
+	if (!message.content.startsWith(ayarlar.prefix) || message.author.bot) return;
+
+	if (message.content.startsWith(ayarlar.prefix + "medgfx")) {
+		message.channel.send("MEDİUM GFX TALEBİNİZ ALINMIŞTIR ! SERDARÜNLÜ İLE İLETİŞİME GEÇİNİZ.");
+	} else
+
+	if (message.content.startsWith(ayarlar.prefix + "foo")) {
+		message.channel.send("bar!");
+	}
+});
+
+  client.on("error", (e) => console.error(e));
+  client.on("warn", (e) => console.warn(e));
+  client.on("debug", (e) => console.info(e));
