c5167f3a68cb396b312dd8921d4f86b63df3b09e
kelbuba123 <43009695+kelbuba123@users.noreply.github.com>
2018-09-08 18:40:16 +0300

    Delete guildMemberAdd.js

--- events/guildMemberAdd.js
+++
@@ -1,6 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Hoş geldin **' + username + '**!');
-    member.guild.defaultChannel.send('hg '+username+'');
-};
-
