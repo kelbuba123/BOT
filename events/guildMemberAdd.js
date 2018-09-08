7b1359c8dc1123fdbc44dcd2d17624df3ba33a83
kelbuba123 <43009695+kelbuba123@users.noreply.github.com>
2018-09-08 12:30:32 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,23 +1,5 @@
-const getDefaultChannel = async (guild) => {
-  // get "original" default channel
-  if(guild.channels.has(guild.id))
-    return guild.channels.get(guild.id)
-
-  // Check for a "general" channel, which is often default chat
-  if(guild.channels.exists("name", "general"))
-    return guild.channels.find("name", "general");
-  // Now we get into the heavy stuff: first channel in order where the bot can speak
-  // hold on to your hats!
-  return guild.channels
-   .filter(c => c.type === "text" &&
-     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-   .sort((a, b) => a.position - b.position ||
-     Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-   .first();
-}
-
-// This is called as, for instance:
-client.on("guildMemberAdd", member => {
-  const channel = getDefaultChannel(member.guild);
-  channel.send(`HoşGeldin ! ${member} GfxYaptırmaya Başla ! Komutlarımız:gfxiste progfx`);
-});
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Hoş geldin **' + username + '**!');
+    member.guild.sendMessage('HoşGeldin  '+username+'İyi Gfxler Yaptırman Dileğiyle ! REVO');
+};
